0192c15e820700b81370ad1956dd62c2ecf952b2
Iago Viotti <iago.viotti@gmail.com>
2022-10-10 15:06:34 -0300

    iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <switch>
+          <route exact path="/" component={ login } />
+        </switch>
+      </BrowserRouter>
+    );
   }
 }
 
